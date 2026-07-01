1e0ee52a60dfc1d386058807f0d526ac5115d161
the man of the hour <him@localhost>
2026-07-01 17:12:32 -0600

    switch to foot

--- cli/terminal.nix
+++ cli/terminal.nix
@@ -1,48 +1,37 @@
 { ... }:
 
 {
-  programs.alacritty = {
+  programs.foot = {
     enable = true;
+    server.enable = true;
     settings = {
-      colors = {
-        primary = {
-          background = "#32302f";
-          foreground = "#ebdbb2";
-        };
-        bright = {
-          black = "#928374";
-          blue = "#83a598";
-          cyan = "#8ec07c";
-          green = "#b8bb26";
-          magenta = "#d3869b";
-          red = "#fb4934";
-          white = "#fbf1c7";
-          yellow = "#fabd2f";
-        };
-        normal = {
-          black = "#3c3836";
-          blue = "#458588";
-          cyan = "#689d6a";
-          green = "#98971a";
-          magenta = "#b16286";
-          red = "#cc241d";
-          white = "#a89984";
-          yellow = "#d79921";
-        };
+      main = {
+        font = "CaskaydiaCove Nerd Font:size=12";
+        term = "xterm-256color";
+        pad = "10x10 center";
       };
-      font = {
-        size = 12;
-        normal.family = "CaskaydiaCove Nerd Font";
+      colors-dark = {
+        alpha = "0.9";
+        background = "32302f";
+        foreground = "ebdbb2";
+        regular0 = "3c3836";
+        regular1 = "cc241d";
+        regular2 = "98971a";
+        regular3 = "d79921";
+        regular4 = "458588";
+        regular5 = "b16286";
+        regular6 = "689d6a";
+        regular7 = "a89984";
+        bright0 = "928374";
+        bright1 = "fb4934";
+        bright2 = "b8bb26";
+        bright3 = "fabd2f";
+        bright4 = "83a598";
+        bright5 = "d3869b";
+        bright6 = "8ec07c";
+        bright7 = "fbf1c7";
       };
-      window = {
-        decorations = "None";
-        dynamic_padding = true;
-        padding = {
-          x = 10;
-          y = 10;
-        };
-        opacity = 0.9;
-      };
+      csd.preferred = "none";
     };
   };
 }
