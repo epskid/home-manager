042a19bcc014930fe02a72f7d57bf3bf4d421018
the man of the hour <him@localhost>
2026-07-01 10:17:16 -0600

    update flakes

--- cli/helix.nix
+++ cli/helix.nix
@@ -15,6 +15,15 @@
       svelte-language-server
       haskell-language-server
     ];
+    languages.language-server.tinymist = {
+      command = "tinymist";
+      config.preview.background.enabled = true;
+      config.preview.background.args = [
+        "--data-plane-host=127.0.0.1:0"
+        "--invert-colors=never"
+        "--open"
+      ];
+    };
     settings = {
       theme = "gruvbox_dark_soft_transparent";
       editor.cursor-shape.insert = "bar";
