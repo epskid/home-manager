a66df2a03dd8fc46c3e5e297136f197a3b6a0c5c
the man of the hour <him@localhost>
2026-07-14 21:59:57 -0600

    ghostty

--- cli/terminal.nix
+++ cli/terminal.nix
@@ -1,37 +1,49 @@
 { ... }:
 
 {
-  programs.foot = {
+  programs.ghostty = {
     enable = true;
-    server.enable = true;
     settings = {
-      main = {
-        font = "CaskaydiaCove Nerd Font:size=12";
-        term = "xterm-256color";
-        pad = "10x10 center";
-      };
-      colors-dark = {
-        alpha = "0.9";
+      theme = "Gruvbox Dark Soft";
+      font-size = 12;
+      font-family = "CaskaydiaCove Nerd Font";
+      background-opacity = 0.9;
+      window-decoration = "none";
+      window-padding-x = 10;
+      window-padding-y = 10;
+      window-padding-balance = true;
+
+      env = "TERM=xterm-256color";
+      working-directory = "home";
+      window-inherit-working-directory = false;
+      quit-after-last-window-closed = false;
+    };
+    themes = {
+      "Gruvbox Dark Soft" = {
         background = "32302f";
         foreground = "ebdbb2";
-        regular0 = "3c3836";
-        regular1 = "cc241d";
-        regular2 = "98971a";
-        regular3 = "d79921";
-        regular4 = "458588";
-        regular5 = "b16286";
-        regular6 = "689d6a";
-        regular7 = "a89984";
-        bright0 = "928374";
-        bright1 = "fb4934";
-        bright2 = "b8bb26";
-        bright3 = "fabd2f";
-        bright4 = "83a598";
-        bright5 = "d3869b";
-        bright6 = "8ec07c";
-        bright7 = "fbf1c7";
+        cursor-color = "ebdbb2";
+        palette = [
+          "0=#3c3836"
+          "1=#cc241d"
+          "2=#98971a"
+          "3=#d79921"
+          "4=#458588"
+          "5=#b16286"
+          "6=#689d6a"
+          "7=#a89984"
+          "8=#928374"
+          "9=#fb4934"
+          "10=#b8bb26"
+          "11=#fabd2f"
+          "12=#83a598"
+          "13=#d3869b"
+          "14=#8ec07c"
+          "15=#fbf1c7"
+        ];
+        selection-foreground = "32302f";
+        selection-background = "ebdbb2";
       };
-      csd.preferred = "none";
     };
   };
 }
