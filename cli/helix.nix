7869365af98689ea2bd3fe5de69a726d4a680a05
the man of the hour <him@localhost>
2026-09-09 11:12:23 -0600

    chorechorechor

--- cli/helix.nix
+++ cli/helix.nix
@@ -15,13 +15,28 @@
       svelte-language-server
       haskell-language-server
     ];
-    languages.language-server.tinymist = {
-      command = "tinymist";
-      config.preview.background.enabled = true;
-      config.preview.background.args = [
-        "--data-plane-host=127.0.0.1:0"
-        "--invert-colors=never"
-        "--open"
+    languages = {
+      language-server.tinymist = {
+        command = "tinymist";
+        config.preview.background.enabled = true;
+        config.preview.background.args = [
+          "--data-plane-host=127.0.0.1:0"
+          "--invert-colors=never"
+          "--open"
+        ];
+      };
+      language-server.pyright = {
+        command = "pyright-langserver";
+        args = [ "--stdio" ];
+      };
+      language = [
+        {
+          name = "python";
+          language-id = "python";
+          roots = [ "uv.lock" ];
+          auto-format = true;
+          language-servers = [ "pyright" ];
+        }
       ];
     };
     settings = {
